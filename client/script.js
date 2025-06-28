faedebb597603143cbf4c89dcd25893fbdfbe994
Dhruv Mittal <125351207+Dhruvdevel@users.noreply.github.com>
2025-06-28 14:05:17 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -18,6 +18,22 @@ function getResults() {
   socket.emit("getResults");
 }
 
+function joinSession() {
+  const sessionId = document.getElementById("session-id").value.trim();
+  const name = document.getElementById("student-name").value.trim();
+  const id = document.getElementById("student-id").value.trim();
+
+  if (!sessionId || !name || !id) return alert("Fill all fields");
+  socket.emit("join", { sessionId, name, id });
+}
+
+
+
+socket.on("eliminated", () => {
+  alert("❌ You have been eliminated!");
+  document.getElementById("vote-section").style.display = "none";
+});
+
 
 
 socket.on("survivors", (list) => {
